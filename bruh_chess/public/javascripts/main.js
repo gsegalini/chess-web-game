f89b47de4c2d59de66efd48f65737eeffa56c999
kazemaksOG <davis.kazemaks@gmail.com>
2021-01-12 00:37:07 +0100

    making the game interactive and splitting up the code

--- bruh_chess/public/javascripts/main.js
+++ bruh_chess/public/javascripts/main.js
@@ -8,9 +8,8 @@
 let activeMatch;
 
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
   activeMatch = new Match();
-
   draw(activeMatch);
 
 });
@@ -22,30 +21,34 @@ function draw(match) {
   let x = 0;
   let y = 0;
 
-  for(row of match.board) {
+  for (row of match.board) {
 
     const htmlRow = document.createElement("div");
     htmlRow.classList.add("row");
-
     y = 0;
-    for(column of row) {
+
+    for (column of row) {
       // makes blocks
       const htmlColumn = document.createElement("div");
       htmlColumn.classList.add("column");
-      if(i % 2 == 0) {
+      const id = String(x) + String(y);
+      htmlColumn.setAttribute("id", id);
+      if (i % 2 == 0) {
         htmlColumn.classList.add("white-block");
       }
 
       // makes piece
-      if(column != "") {
+      if (column != "") {
         const htmlImage = document.createElement("IMG");
         htmlImage.classList.add("piece");
         htmlImage.style.position = "absolute";
         htmlImage.style.zIndex = "10"
 
-        htmlImage.style.left = x*75 + "px";
-        htmlImage.style.top = y*75 + "px";
-        
+        htmlImage.style.left = x * 75 + "px";
+        htmlImage.style.top = y * 75 + "px";
+
+        column.htmlPosition = [(x * 75 + "px"), (y * 75 + "px")];
+
         column.htmlRef = htmlImage;
 
         const color = column.color;
@@ -57,37 +60,113 @@ function draw(match) {
         htmlImage.setAttribute("src", loc);
         htmlImage.setAttribute('draggable', false);
 
+        // Only active pieces get listeners
+        if (column.color == "white") {
 
+          // Click and hold
+          htmlImage.addEventListener("mousedown", function () {
+            const piece = match.myPieces.find((x) => {
+              return x.name == htmlImage.classList[1];
+            })
 
-        htmlImage.addEventListener("mousedown", function() {
+            // disables focus for all
+            match.myPieces.forEach((elem) => {
+              elem.htmlRef.parentElement.classList.remove("focused")
+            });
+
+            // deletes movable
+            match.movableLocation.forEach((id) => {
+              document.getElementById(id).innerHTML = "";
+            })
+            match.movableLocation = [];
+
+            // Draws points to which the piece can move
+            const moves = piece.getMoves();
+            for (let index = 0; index < moves.length; index++) {
+
+              const container = document.createElement("div");
+              container.classList.add("container-movable");
+
+              const movable = document.createElement("div");
+              movable.classList.add("movable");
+              container.appendChild(movable);
+
+              const id = String(moves[index][0]) + String(moves[index][1]);
+              match.movableLocation.push(id);
+
+              document.getElementById(id).appendChild(container);
+            }
+
             htmlImage.parentElement.classList.add("focused");
-          htmlImage.style.zIndex = "1000"
-          match.pieceHeld = htmlImage.classList[1];
-        }, true)
-        
-        htmlImage.addEventListener("mouseup", function(e) {
-          htmlImage.parentElement.classList.remove("focused");
-          htmlImage.style.zIndex = "10"
-          match.pieceHeld = "";
-        }, true)
-        
-        htmlImage.addEventListener('mousemove', function(event) {
-          event.preventDefault();
-          const offset = htmlBoard.getBoundingClientRect();
-          if (match.pieceHeld == htmlImage.classList[1]) {
-            const xCord = event.pageX - offset.left - 37;
-            const yCord = event.pageY - offset.top - 45;
-            htmlImage.style.left = xCord + "px";
-            htmlImage.style.top = yCord + "px";
-          }
-      }, true);
+            htmlImage.style.zIndex = "1000"
+            match.pieceHeld = htmlImage.classList[1];
+          }, true)
 
 
+          // let go of click
+          htmlImage.addEventListener("mouseup", function (event) {
+            const piece = match.myPieces.find((x) => {
+              return x.name == htmlImage.classList[1];
+            })
+            const offset = htmlBoard.getBoundingClientRect();
+            const xCord = event.clientX - offset.left;
+            const yCord = event.clientY - offset.top;
+            
+            const position = getBoardPosition(xCord, yCord);
+            
+            const moves = piece.getMoves();
+            for (let index = 0; index < moves.length; index++) {
+              const id = String(moves[index][0]) + String(moves[index][1]);
+              if(id == position) {
+                const normalCord = normalizeCoordinates(xCord, yCord);
+                
+                piece.htmlPosition = normalCord;
+                piece.increaseMoved();
+                
+                match.board[piece.position[0]][piece.position[1]] = "";
+                piece.setPosition(moves[index][0], moves[index][1])
+                match.board[piece.position[0]][piece.position[1]] = piece.name;
 
+                match.movableLocation.forEach((id) => {
+                  document.getElementById(id).innerHTML = "";
+                })
+                match.movableLocation = [];
+                break;
+              }
+            }
+            
+            htmlImage.style.zIndex = "10"
+            match.pieceHeld = "";
+            htmlImage.style.left = piece.htmlPosition[0];
+            htmlImage.style.top = piece.htmlPosition[1];
+          }, true)
+
+          // move around while holding
+          htmlImage.addEventListener('mousemove', function (event) {
+            event.preventDefault();
+            const offset = htmlBoard.getBoundingClientRect();
+            if (match.pieceHeld == htmlImage.classList[1]) {
+              const xCord = event.clientX - offset.left - 37;
+              const yCord = event.clientY - offset.top - 45;
+              if (xCord < -40 || xCord > 570 || yCord > 580 || yCord < -40) {
+                const piece = match.myPieces.find((x) => {
+                  return x.name == htmlImage.classList[1];
+                })
+                htmlImage.style.zIndex = "10"
+                match.pieceHeld = "";
+                htmlImage.style.left = piece.htmlPosition[0];
+                htmlImage.style.top = piece.htmlPosition[1];
+              } else {
+                htmlImage.style.left = xCord + "px";
+                htmlImage.style.top = yCord + "px";
+              }
+            }
+          }, true);
+        }
 
         htmlColumn.appendChild(htmlImage);
       }
-      
+
 
       htmlRow.appendChild(htmlColumn);
       i++;
@@ -103,270 +182,73 @@ function draw(match) {
 
 
 function Match() {
-  this.board = createBoard();
+  this.myPieces = [];
+  this.opponentPieces = [];
+
+  this.board = createBoard(this.myPieces, this.opponentPieces);
   setupPieces(this.board);
 
-  this.getPiece = function(coords){
+  this.getPiece = function (coords) {
     return this.board[coords[0]][coords[1]];
-}
+  }
 
   this.moveHistory = [];
-  this.myAlivePieces = [];
-  this.opponentAlivePieces = [];
   this.myDeadPieces = [];
   this.opponentDeadPieces = [];
 
   this.pieceHeld = "";
+  this.movableLocation = [];
 
 }
 
+function getBoardPosition(x, y) {
+  return String(Math.floor(x/75)) + String(Math.floor(y/75));
+}
 
+function normalizeCoordinates(x, y) {
+  const res = [];
+  let realX = 0;
+  while(realX + 75 < x) {
+    realX+=75;
+  }
+  let realY = 0;
+  while(realY +75 < y) {
+    realY+=75;
+  }
+  realX+= "px";
+  realY += "px";
+  res.push(realX,realY);
 
+  return res;
+}
 
-
-function setupPieces(board){
-    for (var x = 0;x<8;x++){
-        for (var y = 0;y<8;y++){
-            if (board[x][y] != undefined && board[x][y] != ""){
-                board[x][y].setBoard(board);
-            }
-        }
+function setupPieces(board) {
+  for (var x = 0; x < 8; x++) {
+    for (var y = 0; y < 8; y++) {
+      if (board[x][y] != undefined && board[x][y] != "") {
+        board[x][y].setBoard(board);
+      }
     }
+  }
 }
 
-function createBoard(){
-    let board = 
-    [["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""]];
-    for (let x = 0;x<8;x++){
-        board[x][1] = new pawn("pb"+x, "black", [x,1]);   // Why are these numbered backwards?, because they are numbered "correctly" if we look at them from their side
-        board[x][6] = new pawn("pw"+x, "white", [x,6]);
-    }
-    board[0][0] = new rook("rb1", "black", [0,0]);
-    board[7][0] = new rook("rb0", "black", [7,0]);
-    board[0][7] = new rook("rw0", "white", [0,7]);
-    board[7][7] = new rook("rw1", "white", [7,7]);
-    board[1][0] = new knight("kb1", "black", [1,0]);
-    board[6][0] = new knight("kb0", "black", [6,0]);
-    board[1][7] = new knight("kw0", "white", [1,7]);
-    board[6][7] = new knight("kw1", "white", [6,7]);
-    board[2][0] = new bishop("bb1", "black", [2,0]);
-    board[5][0] = new bishop("bb0", "black", [5,0]);
-    board[2][7] = new bishop("bw0", "white", [2,7]);
-    board[5][7] = new bishop("bw1", "white", [5,7]);
-    board[4][7] = new king("kw", "white", [4,7]);    // I swapped both kings and q
-    board[3][7] = new queen("qw", "white", [3,7]);
-    board[4][0] = new king("kb", "black", [4,0]);
-    board[3][0] = new queen("qb", "black", [3,0]);
-    return board;
-}
+
 
 
 // TODO finish move validation for each piece, for strange rules if we want.
-function gamePiece(name, color, moveFunction, startPosition, board){
-    this.name = name;
-    this.color = color;
-    this.move = moveFunction;
-    this.moved = 0;
-    this.position = startPosition;
-    this.board = null;
-    this.htmlRef = null;
+function gamePiece(name, color, moveFunction, startPosition, board) {
+  this.name = name;
+  this.color = color;
+  this.move = moveFunction;
+  this.moved = 0;
+  this.position = startPosition;
+  this.board = null;
+  
+  this.htmlRef = null;
+  this.htmlPosition = [];
 
-    this.increaseMoved = function(){this.moved++};
-    this.setPosition = function(x,y){this.position = [x,y]};
-    this.getMoves = function(){return this.move(this.position[0], this.position[1], this.color, this.moved)};
-    this.setBoard = function(board){this.board = board};
-}
-
-function pawnMove(x, y, color, moved){
-    let possibles = [];
-    if (color === "black"){
-        if (this.board[x][y+1] === undefined || this.board[x][y+1] === "") possibles.push([x, y+1]);     // When i did testing undefined did not count as "" ye i know, i corrected it only in latters
-        if (moved === 0)
-            if (this.board[x][y+2] === undefined || this.board[x][y+2] === "") possibles.push([x, y+2]);
-        if ((x+1) < 8 && this.board[x+1][y+1] != undefined && this.board[x+1][y+1] != "" && this.board[x+1][y+1].color != color){
-            possibles.push[x+1, y+1];
-        }
-        if ((x-1) >= 0 && this.board[x-1][y+1] != undefined && this.board[x-1][y+1] != "" && this.board[x-1][y+1].color != color){
-            possibles.push[x-1, y+1];
-        }
-    }
-    else{
-        if (this.board[x][y-1] === undefined || this.board[x][y-1] === "") possibles.push([x, y-1]);
-        if (moved === 0)
-            if (this.board[x][y-2] === undefined || this.board[x][y-2] === "") possibles.push([x, y-2]);
-        if ((x+1) < 8 && this.board[x+1][y-1] != undefined && this.board[x+1][y-1] != "" && this.board[x+1][y-1].color != color){
-            possibles.push[x+1, y-1];
-        }
-        if ((x-1) >= 0 && this.board[x-1][y-1] != undefined && this.board[x-1][y-1] != "" && this.board[x-1][y-1].color != color){
-            possibles.push[x-1, y-1];
-        }
-    }
-
-    return possibles;
-}
-
-function pawn(name, color, startPosition){
-    return new gamePiece(name, color, pawnMove, startPosition)
-}
-
-
-function bishopMove(x, y, color, _moved){
-    let possibles = [];
-    let pairs = [[1,1], [1,-1], [-1,1], [-1,-1]];
-
-    for (var i = 0;i<4;i++){
-        for (var k = 1;k<8;k++){
-            let actual = [x+k*pairs[i][0], y+k*pairs[i][1]];
-            if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0){ //move is in the grid
-                var rx = actual[0];
-                var ry = actual[1];
-                if (this.board[rx][ry] != undefined && this.board[rx][ry] != ""){//check the color
-                    if (this.board[rx][ry].color === color) break; //same color, break
-                    else {possibles.push(actual); break;}
-                }
-                possibles.push(actual);
-            }
-        }
-    }
-    return possibles;
-}
-
-function bishop(name, color, startPosition){
-    return new gamePiece(name, color, bishopMove, startPosition);
-}
-
-function rookMove(x, y, color, _moved){
-    let possibles = [];
-    let pairs = [[1,0], [-1,0], [0,1], [0,-1]];
-
-    for (var i = 0;i<4;i++){
-        for (var k = 1;k<8;k++){
-            let actual = [x+k*pairs[i][0], y+k*pairs[i][1]];
-            if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0){ //move is in the grid
-                var rx = actual[0];
-                var ry = actual[1];
-                if (this.board[rx][ry] != undefined && this.board[rx][ry] != ""){//check the color
-                    if (this.board[rx][ry].color === color) break; //same color, break
-                    else {possibles.push(actual); break;}
-                }
-                possibles.push(actual);
-            }
-        }
-    }
-    return possibles;
-}
-
-function rook(name, color, startPosition){
-    return new gamePiece(name, color, rookMove, startPosition);
-}
-
-function knightMove(x, y, color, _moved){
-    let possibles = [];
-    let pairs = [[2,1], [1,2], [2,-1], [1,-2], [-1,-2], [-2,-1],[-2,1],[-1,2]];
-
-    for (var i = 0;i<8;i++){
-        let actual = [x+pairs[i][0], y + pairs[i][1]];
-        if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0){ //on the grid, check color
-            var rx = actual[0];
-            var ry = actual[1];
-            if (this.board[rx][ry] != undefined && this.board[rx][ry] != ""){ //color check
-                if (this.board[rx][ry].color === color) {continue;}
-            }
-            possibles.push(actual);
-        }
-    }
-    return possibles;
-}
-
-function knight(name, color, startPosition){
-    return new gamePiece(name, color, knightMove, startPosition);
-}
-
-function kingMove(x, y, color, _moved){
-    let possibles = [];
-    let pairs = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1],[0,-1],[1,-1]];
-    for (var i = 0;i<8;i++){
-        let actual = [x+pairs[i][0], y + pairs[i][1]];
-        if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0){ //on the grid, check color
-            var rx = actual[0];
-            var ry = actual[1];
-            if (this.board[rx][ry] != undefined && this.board[rx][ry] != ""){ //color check
-                if (this.board[rx][ry].color === color) {continue;}
-            }
-            possibles.push(actual);
-        }
-    }
-
-    if(_moved === 0) {
-        if(color === "white") {
-            if(x == 4 && y == 7) {
-                if(this.board[7][7].name === "rw1" && this.board[7][7].moved === 0) {
-                    if(this.board[6][7] == "" && this.board[5][7] == "") {
-                        console.log("castle kingside!");
-                        possibles.push([6,7]);
-                    }
-                } 
-                if(this.board[0][7].name === "rw0" && this.board[0][7].moved === 0) {
-                    if(this.board[1][7] == "" && this.board[2][7] == "" && this.board[3][7] == "") {
-                        console.log("castle queenside!");
-                        possibles.push([2,7]);
-                    }
-                }
-            }
-
-        } else {
-            if(x == 4 && y == 0) {
-                if(this.board[7][0].name === "rb0" && this.board[7][0].moved === 0) {
-                    if(this.board[6][0] == "" && this.board[5][0] == "") {
-                        console.log("castle kingside!");
-                        possibles.push([6,0]);
-                    }
-                } 
-                if(this.board[0][0].name === "rb1" && this.board[0][0].moved === 0) {
-                    if(this.board[1][0] == "" && this.board[2][0] == "" && this.board[3][0] == "") {
-                        console.log("castle queenside!");
-                        possibles.push([2,0]);
-                    }
-                }
-            }
-        }
-    }
-
-
-    return possibles;
-}
-
-function king(name, color, startPosition){
-    return new gamePiece(name, color, kingMove, startPosition);
-}
-
-function queenMove(x, y, color, _moved){
-    let possibles = [];         //check verticals and the diagonals in the same way as always, just double the pairs
-    let pairs = [[1,0], [-1,0], [0,1], [0,-1], [1,1], [1,-1], [-1,1], [-1,-1]];
-    for (var i = 0;i<8;i++){    
-        for (var k = 1;k<8;k++){
-            let actual = [x+k*pairs[i][0], y+k*pairs[i][1]];
-            if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0){ //move is in the grid
-                var rx = actual[0];
-                var ry = actual[1];
-                if (this.board[rx][ry] != undefined && this.board[rx][ry] != ""){//check the color
-                    if (this.board[rx][ry].color === color) break; //same color, break
-                    else {possibles.push(actual); break;}
-                }
-                possibles.push(actual);
-            }
-        }
-    }
-    return possibles;
-}
-
-function queen(name, color, startPosition){
-    return new gamePiece(name, color, queenMove, startPosition);
+  this.increaseMoved = function () { this.moved++ };
+  this.setPosition = function (x, y) { this.position = [x, y] };
+  this.getMoves = function () { return this.move(this.position[0], this.position[1], this.color, this.moved) };
+  this.setBoard = function (board) { this.board = board };
 }
---
+++ bruh_chess/public/javascripts/moves.js
@@ -0,0 +1,244 @@
+function pawnMove(x, y, color, moved) {
+  let possibles = [];
+  if (color === "black") {
+    if (this.board[x][y + 1] === undefined || this.board[x][y + 1] === "") possibles.push([x, y + 1]);     // When i did testing undefined did not count as "" ye i know, i corrected it only in latters
+    if (moved === 0)
+      if (this.board[x][y + 2] === undefined || this.board[x][y + 2] === "") possibles.push([x, y + 2]);
+    if ((x + 1) < 8 && this.board[x + 1][y + 1] != undefined && this.board[x + 1][y + 1] != "" && this.board[x + 1][y + 1].color != color) {
+      possibles.push[x + 1, y + 1];
+    }
+    if ((x - 1) >= 0 && this.board[x - 1][y + 1] != undefined && this.board[x - 1][y + 1] != "" && this.board[x - 1][y + 1].color != color) {
+      possibles.push[x - 1, y + 1];
+    }
+  }
+  else {
+    if (this.board[x][y - 1] === undefined || this.board[x][y - 1] === "") possibles.push([x, y - 1]);
+    if (moved === 0)
+      if (this.board[x][y - 2] === undefined || this.board[x][y - 2] === "") possibles.push([x, y - 2]);
+    if ((x + 1) < 8 && this.board[x + 1][y - 1] != undefined && this.board[x + 1][y - 1] != "" && this.board[x + 1][y - 1].color != color) {
+      possibles.push[x + 1, y - 1];
+    }
+    if ((x - 1) >= 0 && this.board[x - 1][y - 1] != undefined && this.board[x - 1][y - 1] != "" && this.board[x - 1][y - 1].color != color) {
+      possibles.push[x - 1, y - 1];
+    }
+  }
+
+  return possibles;
+}
+
+function pawn(name, color, startPosition) {
+  return new gamePiece(name, color, pawnMove, startPosition)
+}
+
+
+function bishopMove(x, y, color, _moved) {
+  let possibles = [];
+  let pairs = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+
+  for (var i = 0; i < 4; i++) {
+    for (var k = 1; k < 8; k++) {
+      let actual = [x + k * pairs[i][0], y + k * pairs[i][1]];
+      if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0) { //move is in the grid
+        var rx = actual[0];
+        var ry = actual[1];
+        if (this.board[rx][ry] != undefined && this.board[rx][ry] != "") {//check the color
+          if (this.board[rx][ry].color === color) break; //same color, break
+          else { possibles.push(actual); break; }
+        }
+        possibles.push(actual);
+      }
+    }
+  }
+  return possibles;
+}
+
+function bishop(name, color, startPosition) {
+  return new gamePiece(name, color, bishopMove, startPosition);
+}
+
+function rookMove(x, y, color, _moved) {
+  let possibles = [];
+  let pairs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+  for (var i = 0; i < 4; i++) {
+    for (var k = 1; k < 8; k++) {
+      let actual = [x + k * pairs[i][0], y + k * pairs[i][1]];
+      if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0) { //move is in the grid
+        var rx = actual[0];
+        var ry = actual[1];
+        if (this.board[rx][ry] != undefined && this.board[rx][ry] != "") {//check the color
+          if (this.board[rx][ry].color === color) break; //same color, break
+          else { possibles.push(actual); break; }
+        }
+        possibles.push(actual);
+      }
+    }
+  }
+  return possibles;
+}
+
+function rook(name, color, startPosition) {
+  return new gamePiece(name, color, rookMove, startPosition);
+}
+
+function knightMove(x, y, color, _moved) {
+  let possibles = [];
+  let pairs = [[2, 1], [1, 2], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]];
+
+  for (var i = 0; i < 8; i++) {
+    let actual = [x + pairs[i][0], y + pairs[i][1]];
+    if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0) { //on the grid, check color
+      var rx = actual[0];
+      var ry = actual[1];
+      if (this.board[rx][ry] != undefined && this.board[rx][ry] != "") { //color check
+        if (this.board[rx][ry].color === color) { continue; }
+      }
+      possibles.push(actual);
+    }
+  }
+  return possibles;
+}
+
+function knight(name, color, startPosition) {
+  return new gamePiece(name, color, knightMove, startPosition);
+}
+
+function kingMove(x, y, color, _moved) {
+  let possibles = [];
+  let pairs = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]];
+  for (var i = 0; i < 8; i++) {
+    let actual = [x + pairs[i][0], y + pairs[i][1]];
+    if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0) { //on the grid, check color
+      var rx = actual[0];
+      var ry = actual[1];
+      if (this.board[rx][ry] != undefined && this.board[rx][ry] != "") { //color check
+        if (this.board[rx][ry].color === color) { continue; }
+      }
+      possibles.push(actual);
+    }
+  }
+
+  if (_moved === 0) {
+    if (color === "white") {
+      if (x == 4 && y == 7) {
+        if (this.board[7][7].name === "rw1" && this.board[7][7].moved === 0) {
+          if (this.board[6][7] == "" && this.board[5][7] == "") {
+            console.log("castle kingside!");
+            possibles.push([6, 7]);
+          }
+        }
+        if (this.board[0][7].name === "rw0" && this.board[0][7].moved === 0) {
+          if (this.board[1][7] == "" && this.board[2][7] == "" && this.board[3][7] == "") {
+            console.log("castle queenside!");
+            possibles.push([2, 7]);
+          }
+        }
+      }
+
+    } else {
+      if (x == 4 && y == 0) {
+        if (this.board[7][0].name === "rb0" && this.board[7][0].moved === 0) {
+          if (this.board[6][0] == "" && this.board[5][0] == "") {
+            console.log("castle kingside!");
+            possibles.push([6, 0]);
+          }
+        }
+        if (this.board[0][0].name === "rb1" && this.board[0][0].moved === 0) {
+          if (this.board[1][0] == "" && this.board[2][0] == "" && this.board[3][0] == "") {
+            console.log("castle queenside!");
+            possibles.push([2, 0]);
+          }
+        }
+      }
+    }
+  }
+
+
+  return possibles;
+}
+
+function king(name, color, startPosition) {
+  return new gamePiece(name, color, kingMove, startPosition);
+}
+
+function queenMove(x, y, color, _moved) {
+  let possibles = [];         //check verticals and the diagonals in the same way as always, just double the pairs
+  let pairs = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+  for (var i = 0; i < 8; i++) {
+    for (var k = 1; k < 8; k++) {
+      let actual = [x + k * pairs[i][0], y + k * pairs[i][1]];
+      if (actual[0] < 8 && actual[0] >= 0 && actual[1] < 8 && actual[1] >= 0) { //move is in the grid
+        var rx = actual[0];
+        var ry = actual[1];
+        if (this.board[rx][ry] != undefined && this.board[rx][ry] != "") {//check the color
+          if (this.board[rx][ry].color === color) break; //same color, break
+          else { possibles.push(actual); break; }
+        }
+        possibles.push(actual);
+      }
+    }
+  }
+  return possibles;
+}
+
+function queen(name, color, startPosition) {
+  return new gamePiece(name, color, queenMove, startPosition);
+}
+
+
+
+function createBoard(my, opponent) {
+  let board =
+    [["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""]];
+  for (let x = 0; x < 8; x++) {
+    let pw = new pawn("pw" + x, "white", [x, 6]);
+    let pb = new pawn("pb" + x, "black", [x, 1]);
+    my.push(pw);
+    board[x][1] = pb;
+    opponent.push(pb);
+    board[x][6] = pw;
+  }
+  let rb1 = new rook("rb1", "black", [0, 0]);
+  let rb0 = new rook("rb0", "black", [7, 0]);
+  let rw0 = new rook("rw0", "white", [0, 7]);
+  let rw1 = new rook("rw1", "white", [7, 7]);
+  let kb1 = new knight("kb1", "black", [1, 0]);
+  let kb0 = new knight("kb0", "black", [6, 0]);
+  let kw0 = new knight("kw0", "white", [1, 7]);
+  let kw1 = new knight("kw1", "white", [6, 7]);
+  let bb1 = new bishop("bb1", "black", [2, 0]);
+  let bb0 = new bishop("bb0", "black", [5, 0]);
+  let bw0 = new bishop("bw0", "white", [2, 7]);
+  let bw1 = new bishop("bw1", "white", [5, 7]);
+  let kw = new king("kw", "white", [4, 7]);
+  let qw = new queen("qw", "white", [3, 7]);
+  let kb = new king("kb", "black", [4, 0]);
+  let qb = new queen("qb", "black", [3, 0]);
+  my.push(rw0, rw1, kw0, kw1, bw0, bw1, kw, qw);
+  opponent.push(rb0, rb1, kb0, kb1, bb0, bb1, kb, qb);
+
+  board[0][0] = rb1;
+  board[7][0] = rb0;
+  board[0][7] = rw0;
+  board[7][7] = rw1;
+  board[1][0] = kb1;
+  board[6][0] = kb0;
+  board[1][7] = kw0;
+  board[6][7] = kw1;
+  board[2][0] = bb1;
+  board[5][0] = bb0;
+  board[2][7] = bw0;
+  board[5][7] = bw1;
+  board[4][7] = kw;
+  board[3][7] = qw;
+  board[4][0] = kb;
+  board[3][0] = qb;
+  return board;
+}
